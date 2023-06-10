7ab1ca4f061fc7fb352a57a793c8870ca4a10f68
MashyBasker <basumaharshi10@gmail.com>
2023-06-10 20:58:18 +0530

    Less Go

--- src/conway.c
+++ src/conway.c
@@ -1,6 +1,113 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
 
-int main() {
-    printf("Hello testing\n");
+#include "conway.h"
+
+int ARENA[HEIGHT][WIDTH] = { 0 };
+
+void generate_random_arena()
+{
+    for(size_t y = 0; y < HEIGHT; y++) {
+        for(size_t x = 0; x < WIDTH; x++) {
+            ARENA[y][x] = rand() % 2;
+        }
+    }
+}
+
+void dead_rules(int y, int x) 
+{   
+    int ones = 0;
+    int change1[3] = { -1, 0, 1 };
+    int change2[3] = { -1, 0, 1 };
+
+    /* Iterating over all neighbours */
+    for(size_t j = 0; j < 3; j++) {
+        for(size_t i = 0; i < 3; i++) {
+            if(i == 0 && j == 0) continue;
+            else {
+                int neighbour = ARENA[y + change1[j]][x + change2[i]];
+                if(neighbour == 1) {
+                    ones++;
+                }
+            }
+        }
+    }
+    /* If the number of alive cells around a dead cell == 3 */
+    if(ones == over_lim) {
+        ARENA[y][x] = 1;
+    }
+}
+
+void alive_rules(int y, int x)
+{
+    int ones = 0;
+    int change1[3] = { -1, 0, 1 };
+    int change2[3] = { -1, 0, 1 };
+
+    /* Iterating over all neighbours */
+    for(size_t j = 0; j < 3; j++) {
+        for(size_t i = 0; i < 3; i++) {
+            if(i == 0 && j == 0) continue;
+            else {
+                int neighbour = ARENA[y + change1[j]][x + change2[i]];
+                if(neighbour == 1) {
+                    ones++;
+                }
+            }
+        }
+    }
+    if(ones == 2 || ones == 3) {
+        ARENA[y][x] = 1;
+    } else {
+        ARENA[y][x] = 0;
+    }
+}
+
+void update_arena()
+{
+    for(size_t y = 0; y < HEIGHT; y++) {
+        for(size_t x = 0; x < WIDTH; x++) {
+            if(ARENA[y][x] == 0) {
+                dead_rules(y, x);
+            } else if(ARENA[y][x] == 1) {
+                alive_rules(y, x);
+            }
+        }
+    }
+}
+
+void delay(long t)
+{
+    for(long i = 0; i < t; i++);
+}
+
+void display_arena()
+{
+    int ret = system("clear");
+    if(ret == -1) {
+        perror("[Error] `system` command unsuccessful");
+    }
+    for(size_t y = 0; y < HEIGHT; y++) {
+        for(size_t x = 0; x < WIDTH; x++) {
+            char c = symbol[ARENA[y][x]];
+            
+            fputc(c, stdout);
+        }
+        fputc('\n', stdout);
+    }
+}
+
+int main()
+{
+    srand(time(0));
+    generate_random_arena();
+    display_arena();
+    for(;;) {
+        update_arena();
+        display_arena();
+        delay(10000000000000000);
+    }
     return 0;
 }
---
+++ src/conway.h
@@ -0,0 +1,9 @@
+
+/* Declaring constants */
+#define WIDTH 50
+#define HEIGHT 30
+
+const char symbol[2] = {' ', '#'};
+const int over_lim = 3;
+const int under_lim = 2;
+
